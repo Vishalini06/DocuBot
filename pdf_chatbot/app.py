b12501bacb3ccdfff3ef23e7ece63a31d87608df
Vishalini <123239936+Vishalini06@users.noreply.github.com>
2025-04-14 23:02:46 +0530

    Update app.py

--- pdf_chatbot/app.py
+++ pdf_chatbot/app.py
@@ -77,7 +77,7 @@ def handle_userinput(user_question):
 # Main Streamlit app
 def main():
     load_dotenv()
-    st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
+    st.set_page_config(page_title="DocuBot: Chat with multiple PDFs", page_icon=":books:")
     st.write(css, unsafe_allow_html=True)
 
     if "conversation" not in st.session_state:
